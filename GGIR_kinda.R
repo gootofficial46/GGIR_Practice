93c6b543a0d2d0dd71487cc73ad888022d8653c1
gootofficial46 <george.annas4321@outlook.com>
2024-12-06 11:42:41 +1100

    Update GGIR_kinda.R

--- GGIR_kinda.R
+++ GGIR_kinda.R
@@ -19,13 +19,14 @@ GGIR(
   mode = 1:6,                      # Run all five GGIR modes
   overwrite = TRUE,                # Overwrite existing output
   do.imp = TRUE,                   # Imputation of non-wear time
+  do.visual = TRUE,                # Generates visual plots in parts 4, 5 and 6
   data_Format = "bin",             # Specifies type of file
     
   # Mode 1 (Pre-processing and calibration)
   do.cal = TRUE,                   # Perform auto-calibration
   do.enmo = TRUE,                  # Calculate ENMO (metric of acceleration)
   do.anglex = TRUE,                # Calculate angle relative to gravity
-  windowsizes = c(5, 900, 3600),   # Indicates the short epoch, long epoch, and non-wear detection
+  windowsizes = c(5, 900, 3600),  # Indicates the short epoch, long epoch, and non-wear detection
   printsummary = TRUE,             # Print summary to console
   
   # Mode 2 (Non-wear time and sleep detection)
@@ -36,24 +37,22 @@ GGIR(
   
   # Mode 3 (Physical activity metrics)
   do.part3 = TRUE,                 # Enable mode 3
-  threshold.lig = c(87.5, 250),    # Light activity thresholds (mg)
-  threshold.mod = c(250, 750),     # Moderate activity thresholds (mg)
-  threshold.vig = c(750, 1500),    # Vigorous activity thresholds (mg)
-  timethreshold = 30,              # 30 minute threshold for angle variation to be below angle threshold
   
   # Mode 4 (Individual reports)
   do.part4 = TRUE,                 # Enable mode 4
-  do.visual = TRUE,                # Generate sleep and activity plots
+  timethreshold = 5,               # 30 minute threshold for angle variation to be below angle threshold
+  HASIB.algo = "Sadeh1994",        # Utilising the Sadeh inactivity bout algorithm
+  Sadeh_axis = "Y",                # Specifying vertical movement for the Sadeh algorithm
   
   # Mode 5 (Group summaries)
   do.part5 = TRUE,                 # Enable mode 5
+  threshold.lig = 87.5,            # Light activity thresholds (mg)
+  threshold.mod = 250,             # Moderate activity thresholds (mg)
+  threshold.vig = 750,             # Vigorous activity thresholds (mg)
   save_ms5rawlevels = TRUE,        # Preparation for Part 6
   save_ms5raw_format = "RData",    # Preparation for Part 6
   
   # Mode 6 (Circadian analysis)
   do.part6 = TRUE,                 # Enable mode 6
-  cosinor = TRUE
+  part6CR = TRUE
 )
-
-# Print message when done
-cat("GGIR processing complete! Check the output folder for results.\n")
